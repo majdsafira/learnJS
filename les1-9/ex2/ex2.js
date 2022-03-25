47cc0a6c85f697a4fce54d1ddf7f28948e758481
majdsafira <majdsafira@gmail.com>
2022-03-25 21:52:43 +0300

    main

---
+++ les1-9/ex2/ex2.js
@@ -0,0 +1,27 @@
+document.querySelector("h1").style.color = "red";
+document.querySelector("h1").style.fontSize= "80px";
+document.querySelector("h1").style.fontWeight= "bold";
+document.querySelector("h1").style.textAlign ="center";
+document.querySelector("h1").style.fontFamily = "arial";
+
+
+console.log("%cHello %cdear %cmaijd", "color: red", "color: green ", "background-color: blue");
+
+console.group("group 1");
+console.log("hello majd");
+console.log("hello majd");
+console.log("hello majd");
+
+console.group();
+console.log("hello majd");
+console.group();
+console.log("hello majd");
+
+
+console.table(["majd","ibrahim", "said", "safira"]);
+
+/*
+console.log("Iam In Console");
+document.write("Iam In Page");
+*/
+
---
+++ les1-9/ex3/ex3.js
@@ -0,0 +1,45 @@
+var theTitle = "Majd", 
+theDisc = "Majd web school",
+theTime = "25/10";
+
+
+let p = `<div class = "card">
+    <h3>Hello ${theTitle}</h3>
+    <p>${theDisc} </p>
+    <span>${theTime}</span>
+</div>`
+
+let re = p.repeat(4);
+
+document.write(re);
+
+
+
+
+
+
+
+let numOne = 10,
+numTow = 20;
+
+console.log(numOne, numTow);
+console.log(typeof numOne, typeof numTow);
+
+console.log(`${numOne}${numTow}`);
+console.log(`${typeof numOne} ${typeof numTow}`)
+
+console.log(numOne + "\n"
++ numTow);
+console.log(`${numOne} 
+${numTow}`);
+
+
+
+
+console.log(" \` i,m in \n \/\/ love \/\/ \" \" \" \'\'\' \n \+\+ with \+\+ \n \\ \"\"\" \\ \"\"\" \n \"\" javascript \"\" \`\`");
+
+
+let x = 20, y = 21;
+
+console.log(`_${x}_${x}${y}_${x}${y}_${x}${y}_${y}_`);
+
